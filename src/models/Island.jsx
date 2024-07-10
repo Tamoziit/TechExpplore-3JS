6e37d77a104bb40b7668a1d1c2c5e0eea49eb525
Tamojit Das <tamojitdas181007@gmail.com>
2024-07-10 22:18:02 +0530

    3D models for home

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -15,9 +15,116 @@ import { a } from '@react-spring/three'; //animations
 
 import islandScene from "../assets/3d/island.glb";
 
-const Island = (props) => {
+const Island = ({ isRotating, setIsRotating, ...props }) => {
     const islandRef = useRef();
+
+    const { gl, viewport } = useThree();
     const { nodes, materials } = useGLTF(islandScene);
+
+    const lastX = useRef(0); //last mouse position along x-axis
+    const rotationSpeed = useRef(0);
+    const dampingFactor = 0.95; //determines the speed of the movement of the models on moving the cursor
+
+    //Mouse/touch engaged
+    const handlePointerDown = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        setIsRotating(true);
+
+        const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+        lastX.current = clientX; //assignes current mouse pos. to last mouse pos.
+    }
+    //Mouse/touch disengaged
+    const handlePointerUp = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        setIsRotating(false);
+    }
+    //Mouse/touch in moving condition
+    const handlePointerMove = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+
+        if (isRotating) {
+            const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+            const delta = (clientX - lastX.current) / viewport.width;
+
+            //rotation mechanics
+            islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+            lastX.current = clientX;
+            rotationSpeed.current = delta * 0.01 * Math.PI;
+        }
+    }
+
+    //interaction with keyboard
+    const handleKeyDown = (e) => {
+        if (e.key === 'ArrowLeft') {
+            if (!isRotating) setIsRotating(true);
+            islandRef.current.rotation.y += 0.01 * Math.PI;
+        } else if (e.key === 'ArrowRight') {
+            if (!isRotating) setIsRotating(true);
+            islandRef.current.rotation.y -= 0.01 * Math.PI;
+        }
+    }
+
+    const handleKeyUp = (e) => {
+        if (e.key === 'Arrowleft' || e.key === 'ArrowRight') {
+            setIsRotating(false);
+        }
+    }
+
+    //Applies on every single frame
+    useFrame(() => {
+        if (!isRotating) {
+            rotationSpeed.current *= dampingFactor; //slowing down the rotation;
+
+            if (Math.abs(rotationSpeed.current) < 0.001) {
+                rotationSpeed.current = 0; //static
+            }
+        } else {
+            // When rotating, determine the current stage based on island's orientation
+            const rotation = islandRef.current.rotation.y;
+
+            const normalizedRotation =
+                ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+            // Set the current stage based on the island's orientation
+            switch (true) {
+                case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+                    setCurrentStage(4);
+                    break;
+                case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+                    setCurrentStage(3);
+                    break;
+                case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+                    setCurrentStage(2);
+                    break;
+                case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+                    setCurrentStage(1);
+                    break;
+                default:
+                    setCurrentStage(null);
+            }
+        }
+    })
+
+    useEffect(() => { //handle all the screen effects on user interaction
+        const canvas = gl.domElement;
+        canvas.addEventListener("pointerdown", handlePointerDown);
+        canvas.addEventListener("pointerup", handlePointerUp);
+        canvas.addEventListener("pointermove", handlePointerMove);
+        document.addEventListener("keydown", handleKeyDown);
+        document.addEventListener("keyup", handleKeyUp);
+
+        return () => {
+            canvas.removeEventListener("pointerdown", handlePointerDown);
+            canvas.removeEventListener("pointerup", handlePointerUp);
+            canvas.removeEventListener("pointermove", handlePointerMove);
+            document.removeEventListener("keydown", handleKeyDown);
+            document.removeEventListener("keyup", handleKeyUp);
+        }
+    }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
+
     return (
         <a.group ref={islandRef} {...props}>
             <mesh
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import { Canvas } from "@react-three/fiber"; //A React DOM 3D component Renderer for 3JS
-import { Suspense } from "react"; //for laoding state rebdering
+import { Suspense, useState } from "react"; //for laoding state rebdering
 import Loader from "../components/Loader";
 import Island from "../models/Island";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43]; //x,y,z axes respectively
@@ -12,13 +17,28 @@ const Home = () => {
     if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9]; //smaller scale of the model
     } else {
-      screenScale = [0.9, 0.9, 0.9]; //full scale of the model
+      screenScale = [1, 1, 1]; //full scale of the model
     }
 
     return [screenScale, screenPosition, rotation];
   }
 
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [1, 1, 1];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  }
+
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
@@ -27,21 +47,29 @@ const Home = () => {
       </div>*/}
 
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
         camera={{ near: 0.1, far: 1000 }}
       > {/*objects nearer than 0.1 wrt to camera/view/screen will not be rendered & same for objects farther than 1000*/}
         <Suspense fallback={<Loader />}>
           {/* Lightings */}
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} /> {/* simulate light from distance. eg: sunlight */}
+          <ambientLight intensity={0.5} /> {/* Illuminates all objects equally without casting shadows */}
+          <hemisphereLight skyColor="b1e1ff" groundColor="#000000" intensity={0.8} /> {/* Illuminates the scene with a gradient */}
 
+          <Bird />
+          <Sky />
           <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+          />
+          <Plane
+            planeScale={planeScale}
+            planePosition={planePosition}
+            isRotating={isRotating}
+            rotation={[0, 20, 0]}
           />
         </Suspense>
       </Canvas>
