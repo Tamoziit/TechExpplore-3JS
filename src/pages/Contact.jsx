3d6370b78b4afdb825e23e669e4d1605c47db371
Tamojit Das <tamojitdas181007@gmail.com>
2024-07-11 23:08:39 +0530

    Contacts update:1

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,85 @@
+import { useRef, useState } from "react"
+
 const Contact = () => {
+  const [form, setForm] = useState({ name: '', email: '', message: '' });
+  const [isLoading, setIsLoading] = useState(false);
+  const formRef = useRef(null);
+
+  const handleChange = () => {
+
+  }
+  //Click/typing engaged
+  const handleFocus = () => {
+
+  }
+  //Click/typing disengaged
+  const handleBlur = () => {
+
+  }
+
+  const handleSubmit = () => {
+
+  }
+
   return (
-    <div>Contact</div>
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+
+        <form className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Enter your Name"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="Enter your Email"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Message
+            <textarea
+              name="name"
+              className="textarea"
+              rows={4}
+              placeholder="Enter your Message. Let me know how can I help you!"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
